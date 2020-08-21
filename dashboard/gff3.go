5ff9236dba71fcf5b786015ee14643f7429febd5
Siddhartha Basu <biosidd@gmail.com>
2020-08-21 14:41:00 -0500

    refactor: run goroutine for every channel

--- dashboard/gff3.go
+++ dashboard/gff3.go
@@ -244,7 +244,6 @@ func WaitForPipeline(errs ...<-chan error) error {
 // MergeErrors merges multiple channels of errors.
 // Based on https://blog.golang.org/pipelines.
 func MergeErrors(cs ...<-chan error) <-chan error {
-	var wg sync.WaitGroup
 	// We must ensure that the output channel has the capacity to
 	// hold as many errors
 	// as there are error channels.
@@ -255,15 +254,15 @@ func MergeErrors(cs ...<-chan error) <-chan error {
 	// Start an output goroutine for each input channel in cs.  output
 	// copies values from c to out until c is closed, then calls
 	// wg.Done.
-	output := func(c <-chan error) {
-		for n := range c {
-			out <- n
-		}
-		wg.Done()
-	}
+	var wg sync.WaitGroup
 	wg.Add(len(cs))
 	for _, c := range cs {
-		go output(c)
+		go func(c <-chan error) {
+			for v := range c {
+				out <- v
+			}
+			wg.Done()
+		}(c)
 	}
 
 	// Start a goroutine to close out once all the output goroutines
